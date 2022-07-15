f93c5f2dd03f762d49e313b2406c6b010922c132
zizizi199 <noroiandcat@gmail.com>
2022-07-15 14:46:19 +0700

    update

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -20,9 +20,9 @@ export default function List({ lists, setLists }: IProp) {
         })
         setLists(update)
     }
-    const handleDelete = (delStr: string) => {
+    const handleDelete = (e: string) => {
         setLists(lists => {
-            return lists.filter(list => list.work !== delStr)
+            return lists.filter(list => list.work !== e)
         })
     }
     return (
